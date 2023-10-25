808b9e6d73da91d39f27ff8dda669dd01e17fbf9
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-10-26 00:45:45 +0500

    fix

--- App.js
+++ App.js
@@ -1,22 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import ApiComponent from "./component/ApiComponent";
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+      const jsonData = await response.json();
+      const records = Object.entries(jsonData.bpi).map(([key, value]) => ({
+        currency: value.code,
+        rate: value.rate,
+        description: value.description,
+        symbol: value.symbol,
+      }));
+      setData(records);
+      {console.log(data);}
+    } catch (error) {
+      console.error('Error fetching data: ', error);
+    }
+  };
+
+  const renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <Text>
+        {item.currency} - {item.symbol} {item.rate} - {item.description}
+      </Text>
+    </View>
+  );
+
   return (
-		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
-			<ApiComponent />
-			<StatusBar style="auto" />
-		</View>
-	);
-}
+    <View style={styles.container}>
+      
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 220,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
+
+export default App;
